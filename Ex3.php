5a9c0d4eb09f851b3da10fd8b8bead5771fa9a78
LudOkayomPresnoplyuyLyubomudrOkhalnik <ssomethriange@gmail.com>
2025-05-20 22:57:38 +0300

    Update Ex3.php
    Removed commented vars (useless)

--- Ex3.php
+++ Ex3.php
@@ -2,11 +2,6 @@
 
 $x = floatval(readline($prompt="Введите x: "));
 
-/*
-$arcsin = asin($x/(pow($x, 2) + 1));
-$log = log(pow($x, 4)/(1+pow($x, 4)));
-$log10 = log10(abs(pow(2, -$x)*pow($x, 4-$x)));
-*/
 
 if ($x < 1) { // Первая строка
     echo asin($x/(pow($x, 2) + 1));
